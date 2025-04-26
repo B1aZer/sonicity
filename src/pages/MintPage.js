1578ea3fa91b5fc3c0d9b6d1ecdbd3509901caf8
Dmitrii Branitskii <dmitrii.branitskii@gmail.com>
2025-04-26 22:24:54 +0100

    fix ui menu

--- src/pages/MintPage.js
+++ src/pages/MintPage.js
@@ -16,7 +16,7 @@ export class MintPage {
         this.tokensMinted = 0;
         this.maxSupply = 10000;
         this.mintPrice = "0.01"; // ETH
-        this.contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+        this.contractAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
         this.nftCollection = new NFTCollection();
         this.lastMintedTokenId = null;
         this.render();
