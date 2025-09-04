ba7227990a0dae3d4c1b79a014485bd3f073cbe3
Dmitrii Branitskii <dmitrii.branitskii@gmail.com>
2025-09-04 20:17:50 +0100

    Update pine tree locations and fog configuration in constants.js for scene accuracy
    - Adjusted the locations and sizes of several pine tree instances to better fit the scene layout.
    - Enabled fog effects in the Right Pine Forest area for improved atmospheric depth.

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -149,7 +149,7 @@ export const PROPS = {
             
             { location: { x: 119, y: 0, z: -170 }, size: new THREE.Vector3(5.3, 5.3, 5.3), rotation: { x: 0, y: 0.5, z: 0 } },
             // TODO:
-            { location: { x: 52, y: -3, z: -150 }, size: new THREE.Vector3(4.0, 4.0, 4.0), rotation: { x: 0, y: 1.8, z: 0 } },
+            { location: { x: 70, y: -5, z: -200 }, size: new THREE.Vector3(5.5, 5.5, 5.5), rotation: { x: 0, y: -1.8, z: 0 } },
             // right front
             { location: { x: 42, y: 0, z: -24 }, size: new THREE.Vector3(1.6, 1.6, 1.6), rotation: { x: 0, y: 0.7, z: 0 } },
             /*
@@ -251,7 +251,7 @@ export const PROPS = {
             
             { location: { x: 106, y: 0, z: -170 }, size: new THREE.Vector3(4.8, 4.8, 4.8), rotation: { x: 0, y: 0.4, z: 0 } },
             // TODO:
-            { location: { x: 45, y: -12, z: -150 }, size: new THREE.Vector3(5.3, 5.3, 5.3), rotation: { x: 0, y: 1.6, z: 0 } },
+            { location: { x: 58, y: -8, z: -200 }, size: new THREE.Vector3(5.3, 5.3, 5.3), rotation: { x: 0, y: 1.6, z: 0 } },
             // right front
             { location: { x: 38, y: 0, z: -25 }, size: new THREE.Vector3(1.5, 1.5, 1.5), rotation: { x: 0, y: 0.7, z: 0 } },
             
@@ -294,8 +294,9 @@ export const PROPS = {
             { location: { x: 94, y: -5, z: -170  }, size: new THREE.Vector3(6.3, 6.3, 6.3), rotation: { x: 0, y: 0.5, z: 0 } },
             
             { location: { x: 40, y: 2, z: -30 }, size: new THREE.Vector3(1.2, 1.2, 1.2), rotation: { x: 0, y: -0.3, z: 0 } },
-             /*
-            { location: { x: 70, y: 10, z: -60 }, size: new THREE.Vector3(2.0, 2.0, 2.0), rotation: { x: 0, y: 2.0, z: 0 } },
+             // TODO:
+            { location: { x: 48, y: -6, z: -200 }, size: new THREE.Vector3(5.0, 5.0, 5.0), rotation: { x: 0, y: 2.0, z: 0 } },
+            /*
             { location: { x: -75, y: 15, z: -65 }, size: new THREE.Vector3(2.2, 2.2, 2.2), rotation: { x: 0, y: 1.6, z: 0 } },
             { location: { x: 75, y: 12, z: -65 }, size: new THREE.Vector3(2.4, 2.4, 2.4), rotation: { x: 0, y: 0.7, z: 0 } },
             { location: { x: -80, y: 18, z: -70 }, size: new THREE.Vector3(2.1, 2.1, 2.1), rotation: { x: 0, y: 1.4, z: 0 } },
@@ -326,7 +327,7 @@ export const PROPS = {
 export const FOG_CONFIG = {
     RIGHT_PINE_FOREST_FOG: {
         name: 'Right Pine Forest Fog',
-        enabled: false,
+        enabled: true,
         debug: false, // Enable to show wireframe helpers for fog plane positioning
         // Base fog properties
         baseColor: 0xf5efab, // Soft blue-white fog color
