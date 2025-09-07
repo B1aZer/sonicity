b625178ef44d96a5169ed818f005dd5c1ab19565
Dmitrii Branitskii <dmitrii.branitskii@gmail.com>
2025-09-07 20:53:04 +0100

    Update gold address in add-gold.js to reflect new deployment, ensuring accurate resource allocation in the contract.

--- contracts/scripts/add-gold.js
+++ contracts/scripts/add-gold.js
@@ -17,7 +17,7 @@ async function main() {
     const [owner] = await ethers.getSigners();
     console.log(`Using owner address: ${owner.address}`);
 
-    const address = "0x70997970c51812dc3a010c7d01b50e0d17dc79c8";
+    const address = "0x90f79bf6eb2c4f870365e785982e1f101e93b906";
     const goldAmount = 10000; // 10000 gold
     const foodAmount = 1000; // 300 food
     const diamondAmount = 100; // 100 diamonds
