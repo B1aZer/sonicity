bf51f94e50451c34c7c1ea3d3b18f8d0620ac647
Dmitrii Branitskii <dmitrii.branitskii@gmail.com>
2025-09-24 21:31:31 +0100

    Add new tactic images and update image paths in TacticsCenterPage.js: Introduced new PNG files for tactics and updated the image source paths in TacticsCenterPage.js to reflect the new naming conventions, ensuring correct loading of images.

--- src/pages/TacticsCenterPage.js
+++ src/pages/TacticsCenterPage.js
@@ -361,7 +361,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-4">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/cavalry-rush.png" alt="Cavalry Rush" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/CavalryRush.png" alt="Cavalry Rush" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
@@ -391,7 +391,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-5">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/defensive-circle.png" alt="Defensive Circle" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/DefensiveCircle.png" alt="Defensive Circle" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
@@ -421,7 +421,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-6">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/tactical-feint.png" alt="Tactical Feint" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/TacticalFeint.png" alt="Tactical Feint" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
@@ -451,7 +451,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-7">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/swift-strike.png" alt="Swift Strike" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/SwiftStrike.png" alt="Swift Strike" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
@@ -481,7 +481,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-8">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/shadow-guard.png" alt="Shadow Guard" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/ShadowGuard.png" alt="Shadow Guard" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
@@ -511,7 +511,7 @@ export class TacticsCenterPage extends BasePage {
 
                         <div class="shop-item-card" id="tactic-9">
                             <div class="shop-item-image">
-                                <img src="/images/tactics/stealth-trap.png" alt="Stealth Trap" onerror="this.src='/images/tactics/default.png'" />
+                                <img src="/images/tactics/StealthTrap.png" alt="Stealth Trap" onerror="this.src='/images/tactics/default.png'" />
                             </div>
                             <div class="shop-item-info">
                                 <div class="shop-item-title-row">
